0e14ae56d21b701c6a4c2d135e666bc54ba37406
Avadhesh <avadhesh18@users.noreply.github.com>
2022-07-21 19:11:50 +0530

    Update run.php

--- run.php
+++ run.php
@@ -1,3 +1,3 @@
 <?php 
 $rand = rand(1,10000);
-file_put_contents(file_get_contents('https://txti.es/'),$rand,FILE_APPEND);
+file_put_contents('this.txt',file_get_contents('https://txti.es/'),FILE_APPEND);
