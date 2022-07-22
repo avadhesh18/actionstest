4acec7d4f593c84d12171ff7e71c5ed26a81cac5
Avadhesh <avadhesh18@users.noreply.github.com>
2022-07-23 00:01:52 +0530

    Update run.php

--- run.php
+++ run.php
@@ -44,7 +44,7 @@ $data['status'] =  $info['http_code'];
 $data['resp_time'] = $info['total_time'];
 $filepath = 'docs/'.$sitex.'.json';
 file_put_contents($filepath,json_encode($data).PHP_EOL,FILE_APPEND | LOCK_EX);
-    if(count(file($filepath)) > 10){
+    if(count(file($filepath)) > 100){
 $contents = file($filepath);
 $first_line = array_splice($contents, 0, 10);
 file_put_contents($filepath, implode('', $contents));
