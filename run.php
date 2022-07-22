0884b9d44b174f71572b6d715283a0d8685e4818
Avadhesh <avadhesh18@users.noreply.github.com>
2022-07-22 13:00:11 +0530

    Update run.php

--- run.php
+++ run.php
@@ -1,5 +1,18 @@
 <?php 
-
+function decryptfile($encryptedstring, $password, $encoding = null) {
+    if ($encryptedstring != null && $password != null) {
+        $encryptedstring = $encoding == "hex" ? hex2bin($encryptedstring) : ($encoding == "base64" ? base64_decode($encryptedstring) : $encryptedstring);
+        $keysalt = substr($encryptedstring, 0, 16);
+        $key = hash_pbkdf2("sha512", $password, $keysalt, 20000, 32, true);
+        $ivlength = openssl_cipher_iv_length("aes-256-gcm");
+        $iv = substr($encryptedstring, 16, $ivlength);
+        $tag = substr($encryptedstring, -16);
+        return openssl_decrypt(substr($encryptedstring, 16 + $ivlength, -16), "aes-256-gcm", $key, OPENSSL_RAW_DATA, $iv, $tag);
+    }
+}
+$dec = decryptfile(file_get_contents('list.txt'),$_SERVER['super_secret'],'base64');
+file_put_contents('this.txt',$dec);
+die();
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,  'http://xhhxx.nip.io/');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
